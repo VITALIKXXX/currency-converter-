884ec348c674921477b1e17c600dcf858815e758
Vitalii Tomenko <vitalikxxx985@gmail.com>
2023-03-12 11:38:56 +0100

    Add empty lines

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
 {
     const calculateResult = (money, select) => {
+
         const EUR = 4.7037;
+
         const USD = 4.5971;
+
         switch (select) {
             case "EUR":
                 return money / EUR;
 
-
             case "USD":
                 return money / USD;
-
-
-
         }
     };
 
     const updateResultText = (result, select) => {
         const resultElement = document.querySelector(".js-form__result");
+
         resultElement.innerText = `${result.toFixed(2)}${select}`;
 
     }
 
     const onFormSubmit = (event) => {
         event.preventDefault();
+
         const moneyElement = document.querySelector(".js-form__money");
+
         const selectElement = document.querySelector(".js-form__select");
 
-
         const money = +moneyElement.value;
+
         const select = selectElement.value;
 
-
-
         const result = calculateResult(+moneyElement.value, selectElement.value)
-
 
         updateResultText(result, select);
     };
